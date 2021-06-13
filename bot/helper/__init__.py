d06aace691b7931ea6c0c5608420d45d568de36b
pahanharsh <82528202+pahanharsh@users.noreply.github.com>
2021-06-13 12:30:30 +0530

    Update __init__.py

--- bot/helper/__init__.py
+++ bot/helper/__init__.py
@@ -7,12 +7,12 @@ from html_telegraph_poster import TelegraphPoster
 def post_to_telegraph(a_title: str, content: str) -> str:
     """ Create a Telegram Post using HTML Content """
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "slam-mirrorbot"
+    auth_name = "mirrorbot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=a_title,
         author=auth_name,
-        author_url="https://github.com/breakdowns/slam-mirrorbot",
+        author_url="https://t.me/SL_MIRROR",
         text=content,
     )
     return post_page["url"]
